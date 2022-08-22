069a5c3a561a031aecf07e6819b8e9a0902edc3d
miaoyu <miaoyu2009@qq.com>
2022-08-22 08:39:27 +0800

    date

--- 01javascript/60day/40date/index.js
+++ 01javascript/60day/40date/index.js
@@ -35,26 +35,40 @@ console.log(" date.toLocaleString()", date.toLocaleString());
  * 封装一般日期格式 如：2021-7-28 19:17:33
  */
 
-function formatTime(time) {
-  let y = time.getFullYear(); //getFullYear方法以四位数字返回年份
-  let M = time.getMonth() + 1; // getMonth方法从 Date 对象返回月份 (0 ~ 11),需+1
-  let d = time.getDate(); // getDate方法从 Date 对象返回一个月中的某一天 (1 ~ 31)
-  let h = time.getHours(); // getHours方法返回 Date 对象的小时 (0 ~ 23)
-  let m = time.getMinutes(); // getMinutes方法返回 Date 对象的分钟 (0 ~ 59)
-  let s = time.getSeconds(); // getSeconds方法返回 Date 对象的秒数 (0 ~ 59)
+function formatDate(date) {
+  let y = date.getFullYear(); //getFullYear方法以四位数字返回年份
+  let M = date.getMonth() + 1; // getMonth方法从 Date 对象返回月份 (0 ~ 11),需+1
+  let d = date.getDate(); // getDate方法从 Date 对象返回一个月中的某一天 (1 ~ 31)
+  let h = date.getHours(); // getHours方法返回 Date 对象的小时 (0 ~ 23)
+  let m = date.getMinutes(); // getMinutes方法返回 Date 对象的分钟 (0 ~ 59)
+  let s = date.getSeconds(); // getSeconds方法返回 Date 对象的秒数 (0 ~ 59)
   return y + "-" + M + "-" + d + " " + h + ":" + m + ":" + s;
 }
-let time1 = formatTime(new Date());
-console.log(time1); //  2021-7-28 19:17:33
+let date1 = formatDate(new Date());
+console.log(date1); //  2021-7-28 19:17:33
 
 /**
  * 通过new Date() 将时间转化成毫秒数
  * 应用：算两个日期相隔多少天
  */
 
-function IntervalTime(date1, date2) {
-  let num1 = new Date(date1); // 将时间转化为毫秒数
-  let num2 = new Date(date2);
+function IntervalDate(date1, date2) {
+  let num1 = new Date(date1).valueOf(); // 将时间转化为毫秒数
+  let num2 = new Date(date2).valueOf();
   return (num2 - num1) / 1000 / 60 / 60 / 24; // 两个毫秒数相减再取天数
 }
-console.log(IntervalTime("2021-7-2", "2021-7-28")); // 26
+console.log(IntervalDate("2021-7-2", "2021-7-28")); // 26
+
+/* 
+  判断时间是否晚于当前时间
+*/
+
+function laterCurrentDate(date) {
+  let num1 = new Date().valueOf();
+  let num2 = new Date(date).valueOf();
+  if (num1 > num2) {
+    return true;
+  } else {
+    return false;
+  }
+}
